2d30d072f04bbf65a1ddac1d08f1c7398b92a497
horse <horse@SOMEONESLAPTOP>
2023-04-11 10:06:08 -0400

    Started

--- XMLWeather/Form1.cs
+++ XMLWeather/Form1.cs
@@ -13,7 +13,9 @@ namespace XMLWeather
 {
     public partial class Form1 : Form
     {
-        // TODO: create list to hold day objects
+        //list to hold day objects
+        public static List<Day> days = new List<Day>();
+
 
 
         public Form1()
@@ -34,11 +36,24 @@ namespace XMLWeather
 
             while (reader.Read())
             {
-                //TODO: create a day object
+                //creates a day object
+                Day d = new Day();
 
-                //TODO: fill day object with required data
+                //fill day object with required data
+                reader.ReadToFollowing("time");
+                d.date = reader.GetAttribute("day");
 
-                //TODO: if day object not null add to the days list
+                reader.ReadToFollowing("temperature");
+                d.tempLow = reader.GetAttribute("min");
+                d.tempHigh = reader.GetAttribute("max");
+
+                //adds days to the list if not null
+                if (d.date != null)
+                {
+                    days.Add(d);
+                }
+                reader.Close();
+
             }
         }
 
@@ -48,7 +63,11 @@ namespace XMLWeather
             XmlReader reader = XmlReader.Create("http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0");
 
             //TODO: find the city and current temperature and add to appropriate item in days list
+            reader.ReadToFollowing("city");
+            days[0].location = reader.GetAttribute("name");
 
+            reader.ReadToFollowing("temperature");
+            days[0].currentTemp = reader.GetAttribute("value");
         }
 
 
